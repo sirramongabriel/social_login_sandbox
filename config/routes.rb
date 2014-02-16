db2b07f0b2139664508bd3ef07ea50c6f6f50fc0
sirramongabriel <sirramongabriel@gmail.com>
2014-02-15 21:02:52 -0500

    Troubleshoot omniauth'

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 SocialLoginSandbox::Application.routes.draw do
   root to: 'static_pages#index'
 
-  devise_for :users, path_names:  { sign_in: 'login', sign_out: 'logout' } #,
-                     # controllers: { omnniauth_callbacks: 'omnniauth_callbacks' }
+  devise_for :users, path_names:  { sign_in: 'login', sign_out: 'logout' },
+                     controllers: { omnniauth_callbacks: 'omnniauth_callbacks' }
 
   match "users/auth/:provider/callback" => "sessions#create"
   match "/signout" => "sessions#destroy", as: :signout
